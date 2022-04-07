39731e82010a3c60a2a327ad27094b4b79c0b116
Christophe Bedard <bedard.christophe@gmail.com>
2022-04-07 15:15:14 -0400

    Enable kernel network events for trace sync for distributed example
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- examples/example-1_transport_2.launch.py
+++ examples/example-1_transport_2.launch.py
@@ -33,7 +33,11 @@ def generate_launch_description():
             session_name='trace-example-1_single_sync_1-to-1_pingpong_b',
             append_timestamp=True,
             base_path=os.path.dirname(os.path.realpath(__file__)),
-            events_kernel=[],
+            # For trace synchronization
+            events_kernel=[
+                'net_dev_queue',
+                'net_if_receive_skb',
+            ],
             events_ust=[
                 'dds:*',
                 'ros2:*',
